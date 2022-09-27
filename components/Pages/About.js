e30c820de41433ddd9b458a4657d0ecbdc04c7a7
Valentin <valentin.freville97@gmail.com>
2022-09-27 16:33:57 +0200

    Style incroyable sur l'appli

--- components/Pages/About.js
+++ components/Pages/About.js
@@ -1,8 +1,8 @@
-import {Text, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 
 export default function About() {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>A propos de CalculaCat</Text>
             <Text>
                 Salut
@@ -10,3 +10,11 @@ export default function About() {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
